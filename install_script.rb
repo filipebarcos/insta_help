b5f7175b1cf6c6fe9a166c132681f62e60dadfce
Filipe Costa <filipebarcos@gmail.com>
2013-05-20 23:57:41 -0300

    slowly but going ;D

--- install_script.rb
+++ install_script.rb
@@ -2,36 +2,49 @@
 require 'open-uri'
 
 DOWNLOAD_DIR = "~/Downloads"
-class Handler
-  def download application
-    open(File.expand_path(DOWNLOAD_DIR, application.name), 'wb') do |file|
-      file << open(application.url).read
+module Handler
+  class << self
+    def download application
+      filepath = File.join DOWNLOAD_DIR, application.filename
+      open(File.expand_path(filepath), 'wb') do |file|
+        file << open(application.url).read
+      end
+      puts "#{application.name} download has finished..."
+      filepath
     end
-  end
 
-  def mount dmg_file
-    %x(hdiutil mount #{dmg_file})
-  end
+    def mount dmg_file
+      puts "mounting file"
+      %x(hdiutil mount #{dmg_file})
+    end
 
-  def unmount mounted_path
-    %x(hdiutil unmount \"#{mounted_path}\")
-  end
+    def unmount mounted_path
+      %x(hdiutil unmount \"#{mounted_path}\")
+    end
 
-  def unzip zip_file
-    %x(unzip #{zip_file})
-  end
+    def unzip zip_file
+      puts "unziping the file"
+      %x(unzip #{zip_file})
+    end
 
-  def install name, extension
-    #first_step: deal with dmg, zip
-    #second_step: deal with pkg, app
-  end
+    def install extension, filepath
+      case extension
+      when ".dmg"
+        mount filepath
+      when ".zip"
+        unzip filepath
+      end
 
-  def app_command app_path
-    %x(sudo cp -R \"#{app_path}\" /Applications)
-  end
+      #second_step: deal with pkg, app
+    end
 
-  def pkg_command pkg_path
-    %x(sudo installer -package \"#{pkg_path}\" -target "/Volumes/Macintosh HD")
+    def app_command app_path
+      %x(sudo cp -R \"#{app_path}\" /Applications)
+    end
+
+    def pkg_command pkg_path
+      %x(sudo installer -package \"#{pkg_path}\" -target "/Volumes/Macintosh HD")
+    end
   end
 end
 
@@ -46,9 +59,15 @@ class Application
   def filename
     url.split('/').last
   end
+
+  def extension
+    File.extname filename
+  end
 end
 
-applications = [Application.new("dropbox", "https://dl.dropboxusercontent.com/u/17/Dropbox%202.0.16.dmg"),
-                Application.new("", ""),
-                Application.new("", "")
-               ]
+[Application.new("dropbox", "https://dl.dropboxusercontent.com/u/17/Dropbox%202.0.16.dmg")].each do |app|
+  filepath = Handler.download app
+  Handler.install app.extension, filepath
+end
+
+
